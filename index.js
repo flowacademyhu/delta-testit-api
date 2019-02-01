4b953c4551693a236abef1b8510f43cbc33f4bbe
szani <gabor.szaniszlo82@gmail.com>
2019-02-01 14:40:57 +0100

    index.js simplified

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/*app.use('/docs', swaggerUi.serve, swaggerUi.setup(require(swaggerFilePath)));
-
-createMiddleware(swaggerFilePath, app, (err, middleware) => {
-  if (err) return console.log(err);
-  app.use(
-    middleware.metadata(),
-    middleware.CORS(),
-    middleware.files(),
-    middleware.parseRequest(),
-    middleware.validateRequest()
-  );
-  app.get('/test', (req, res) => {
-    res.json({mukodik: true});
-  });
-
-  app.listen(8000);
-});*/
-
 app.use('/users', users);
 
 app.listen(process.env.PORT);
