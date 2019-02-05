8a68d6ba41e564fa1ec57a50c32dbe10abf69b26
szani <gabor.szaniszlo82@gmail.com>
2019-02-05 11:31:16 +0100

    question status field added

--- migrations/0000000000005-create-question.js
+++ migrations/0000000000005-create-question.js
@@ -30,6 +30,10 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false
       },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
