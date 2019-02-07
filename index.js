ef82a67168ee2f00bf491449addcc2c7aa80652a
rartaat <rartaat@gmail.com>
2019-02-07 14:05:58 +0100

    swagger bugfixed

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const tests = require('./controllers/tests');
 const answer = require('./controllers/answer');
 const userResults = require('./controllers/userResults');
 
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerFilePath = './docs/swagger.json';
-// const createMiddleware = require('swagger-express-middleware');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFilePath = './docs/swagger.json';
+const createMiddleware = require('swagger-express-middleware');
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use('/docs', swaggerUi.serve, swaggerUi.setup(require(swaggerFilePath)));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(require(swaggerFilePath)));
 
 app.use('/questions', questions);
 app.use('/users', users);
@@ -25,7 +25,6 @@ app.use('/tests', tests);
 app.use('/answer', answer);
 app.use('/users/:userId/results', userResults);
 
-/*
 createMiddleware(swaggerFilePath, app, (err, middleware) => {
   if (err) return console.log(err);
   app.use(
@@ -36,6 +35,5 @@ createMiddleware(swaggerFilePath, app, (err, middleware) => {
     middleware.validateRequest()
   );
 });
-*/
 
 app.listen(process.env.PORT);
