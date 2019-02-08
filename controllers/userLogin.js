c9c54ce1d4648e22967c0b16a64dc053dafad19e
szani <gabor.szaniszlo82@gmail.com>
2019-02-08 11:26:43 +0100

    userLogin

---
+++ controllers/userLogin.js
@@ -0,0 +1,26 @@
+const models = require('../models');
+const express = require('express');
+const userLogin = express.Router({mergeParams: true});
+const bcrypt = require('bcrypt');
+
+userLogin.post('/', (req, res, next) => {
+  bcrypt.hash(req.body.encryptedPassword, 10, (error, hash) => {
+    if (error) {
+      res.status(500).json(error);
+    } else {
+      models.User.create({
+        role: req.body.role,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email
+        // picture: req.file.path
+      }).then(result => {
+        res.status(201).json({
+          message: 'User created'
+        });
+      }).catch(res.status(500).json(error));
+    }
+  });
+});
+
+module.exports = userLogin;
