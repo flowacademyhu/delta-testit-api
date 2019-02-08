10a4a0c6aa5ab505e10944c2c49da0aca4ada590
rartaat <rartaat@gmail.com>
2019-02-08 17:21:38 +0100

    as

--- index.js
+++ index.js
@@ -32,23 +32,23 @@ createMiddleware(swaggerFilePath, app, (err, middleware) => {
   );
 });
 
-app.use((req, res, next) => {
-  if (req.header('x-auth')) {
-    let token = req.header('x-auth');
-    let decoded;
-    try {
-      decoded = jwt.verify(token, process.env.SECRET);
-    } catch (err) {
-      return res.status(401).send(err);
-    }
-    models.User.findById(decoded.id).then(user => {
-      req.user = user;
-      next();
-    });
-  } else {
-    next();
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.header('x-auth')) {
+//     let token = req.header('x-auth');
+//     let decoded;
+//     try {
+//       decoded = jwt.verify(token, process.env.SECRET);
+//     } catch (err) {
+//       return res.status(401).send(err);
+//     }
+//     models.User.findById(decoded.id).then(user => {
+//       req.user = user;
+//       next();
+//     });
+//   } else {
+//     next();
+//   }
+// });
 
 app.use('/questions', questions);
 app.use('/users', users);
