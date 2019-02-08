418caa116382999f3d58894cd92a5d68fc77a2a2
szani <gabor.szaniszlo82@gmail.com>
2019-02-08 17:26:53 +0100

    time model modified

--- migrations/0000000000007-create-test.js
+++ migrations/0000000000007-create-test.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       time: {
-        type: Sequelize.TIME
+        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
--- models/test.js
+++ models/test.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Test = sequelize.define('Test', {
     userId: DataTypes.INTEGER,
     name: DataTypes.STRING,
-    time: DataTypes.TIME
+    time: DataTypes.INTEGER
   }, {});
   Test.associate = function (models) {
     Test.belongsTo(models.Result, { foreignKey: 'resultId' });
