9a6ba5da5f8719884c2c5cd4b1a5c2fe96917d5d
rartaat <rartaat@gmail.com>
2019-02-08 11:22:17 +0100

    error

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
 const questions = require('./controllers/questions');
 const users = require('./controllers/users');
 const tests = require('./controllers/tests');
 const answer = require('./controllers/answer');
 const userResults = require('./controllers/userResults');
+const models = require('./models');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerFilePath = './docs/swagger.json';
@@ -18,12 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(require(swaggerFilePath)));
 
-app.use('/questions', questions);
-app.use('/users', users);
-app.use('/tests', tests);
-app.use('/answer', answer);
-app.use('/users/:userId/results', userResults);
-
 createMiddleware(swaggerFilePath, app, (err, middleware) => {
   if (err) return console.log(err);
   app.use(
@@ -34,5 +31,29 @@ createMiddleware(swaggerFilePath, app, (err, middleware) => {
     middleware.validateRequest()
   );
 });
+
+app.use((req, res, next) => {
+  if (req.header('x-auth')) {
+    let token = req.header('x-auth');
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.SECRET);
+    } catch (err) {
+      return res.status(401).send(err);
+    }
+    models.User.findById(decoded.id).then(user => {
+      req.user = user;
+      next();
+    });
+  } else {
+    next();
+  }
+});
+
+app.use('/questions', questions);
+app.use('/users', users);
+app.use('/tests', tests);
+app.use('/answer', answer);
+app.use('/users/:userId/results', userResults);
 
 app.listen(process.env.PORT);
