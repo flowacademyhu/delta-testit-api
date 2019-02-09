c0ce4ced5e4a94e847999835aa180c964bf5661a
szani <gabor.szaniszlo82@gmail.com>
2019-02-09 17:07:13 +0100

    merged with develop

--- seeders/20190205173611-tests.js
+++ seeders/20190205173611-tests.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> develop
-'use strict';
-
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Tests', [
-      {
-        userId: 1,
-<<<<<<< HEAD
-        answerId: 1,
-=======
->>>>>>> develop
-        name: 'firstTest',
-        time: 10,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        userId: 2,
-<<<<<<< HEAD
-        answerId: 2,
-=======
->>>>>>> develop
-        name: 'secondTest',
-        time: 15,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        userId: 3,
-<<<<<<< HEAD
-        answerId: 3,
-=======
->>>>>>> develop
-        name: 'thirdTest',
-        time: 30,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]);
-  },
-
-  down: (queryInterface, Sequelize) => {
-  }
-};
-<<<<<<< HEAD
-=======
 'use strict';
 
 module.exports = {
@@ -80,6 +30,3 @@ module.exports = {
   down: (queryInterface, Sequelize) => {
   }
 };
->>>>>>> develop
-=======
->>>>>>> develop
