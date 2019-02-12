dad301bf91eaed2cfcee98dbbb987a823c607346
rartaat <rartaat@gmail.com>
2019-02-12 14:55:21 +0100

    resetPassword

--- controllers/userLogin.js
+++ controllers/userLogin.js
@@ -1,8 +1,9 @@
 const models = require('../models');
 const express = require('express');
-const userLogin = express.Router({mergeParams: true});
+const userLogin = express.Router({ mergeParams: true });
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const generator = require('generate-password');
 
 // userLogin.post('/', (req, res) => {
 //   models.User.findOne({where: {email: req.body.email}})
@@ -21,20 +22,20 @@ const jwt = require('jsonwebtoken');
 // });
 
 userLogin.post('/', (req, res) => {
-  models.User.findOne({where: {email: req.body.email}})
+  models.User.findOne({ where: { email: req.body.email } })
     .then(user => {
       if (!user) {
-        res.status(401).json({message: 'Authentication failed.'});
+        res.status(401).json({ message: 'Authentication failed.' });
       } else {
         bcrypt.compare(req.body.password, user.encryptedPassword, (err, result) => {
           if (err) {
-            res.status(401).json({message: 'Authentication failed.'});
+            res.status(401).json({ message: 'Authentication failed.' });
           } else if (result) {
-            const token = jwt.sign({data: req.body.email}, 'secretpass', {expiresIn: '1h'});
-            res.status(200).json({message: 'Authentication successful.', token});
+            const token = jwt.sign({ data: req.body.email }, 'secretpass', { expiresIn: '1h' });
+            res.status(200).json({ message: 'Authentication successful.', token });
             console.log(token);
           } else {
-            res.status(401).json({message: 'Authentication failed.'});
+            res.status(401).json({ message: 'Authentication failed.' });
           }
           console.log(result);
         });
@@ -42,6 +43,33 @@ userLogin.post('/', (req, res) => {
     })
     .catch(error => {
       res.status(500).json(error);
+    });
+});
+
+userLogin.put('/reset', (req, res) => {
+  models.User.findOne({ where: { email: req.body.email } })
+    .then(user => {
+      if (!user) {
+        res.status(400).json({ message: 'Email does not exist!' });
+      } else {
+        let pwd = generator.generate({
+          length: 10,
+          numbers: true
+        });
+        bcrypt.hash(pwd, 10)
+          .then(hash => {
+            pwd = hash;
+            models.User.update({
+              encryptedPassword: pwd
+            });
+          });
+        let email = {
+          form: 'TestIT group, testIT@gmail.com',
+          to: req.body.email,
+          subject: 'TestIT reset passwowrd',
+          text: 'Tisztelt regisztált tagunk! Az ön általt igényelet új password' + pwd
+        }
+      }
     });
 });
 
