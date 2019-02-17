17368662cfb47f1d0f5875ed440c97e3369fa4df
szani <gabor.szaniszlo82@gmail.com>
2019-02-17 20:27:17 +0100

    results controller prototype modified

--- controllers/results.js
+++ controllers/results.js
@@ -13,6 +13,64 @@ results.get('/', (req, res) => {
 
 // create
 results.post('/', async (req, res) => {
+  let questionsBlock = req.body.questions;            // a tesztben található összes kérdés
+  let answersBlock = req.body.answers;                // a tesztben a található összes kérdés összes lehetséges válasza sorrendben
+  let choosenAnswersBlock = req.body.choosenAnswers;  // a vizsgázó által kiválasztott válaszok
+
+  let testObject = {
+    userId: req.body.userId,
+    testId: req.body.testId,
+    status: req.body.status,
+    archivedTest: {
+      questions: [],
+      pointsAchieved: 0
+    }
+  };
+
+  for (let i = 0; i < questionsBlock.length; i++) {
+    let questionObject = {
+      number: i,
+      answers: [],
+      choosenAnswer: null,
+      subjectId: questionsBlock[i].subjectId,
+      text: questionsBlock[i].text,
+      value: questionsBlock[i].value,
+      pointsGiven: 0
+    };
+    for (let j = i; j <= i + 3; j++) {
+      questionObject.answers.push(answersBlock[j]);
+    }
+    if (choosenAnswersBlock[i].isCorrect) {
+      questionObject.pointsGiven = questionsBlock[i].value;
+      testObject.archivedTest.pointsAchieved += questionsBlock[i].value;
+    }
+    testObject.archivedTest.questions.push(questionObject);
+    testObject.archivedTest.questions[i].choosenAnswer = choosenAnswersBlock[i];
+  }
+
+  let promises = [];
+  models.Result.create({
+    testObject
+  }).then(async result => {
+    result.questions.map(async question => {
+      promises.push(models.choosenAnswer.create(
+        {
+          resultId: result.id,
+          answerId: question.choosenAnswer.id,
+          points: question.pointsGiven
+        }
+      ));
+    });
+
+    let resp = await Promise.all(promises);
+    res.json({resp});
+  }).catch(error => {
+    res.status(404).json(error);
+  });
+});
+
+// create
+results.post('/', async (req, res) => {
   models.Result.create({
     userId: req.body.userId,
     testId: req.body.testId,
@@ -226,10 +284,10 @@ results.post('/', async (req, res) => {
               text: req.body.questions[9].answer4.text
             }}
           ]
+          // chosenAnswer: req.body.questions[9].choosenAnswer,
+          // pointsAchieved:
         }}
       ]
-      // chosenAnswer: req.body.questions[9].choosenAnswer,
-      // pointsAchieved:
     }
   }).then(async result => {
     let promises = [];
