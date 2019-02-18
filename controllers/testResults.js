8e91b684e3aed4a998e487af466c8dcef9689619
szani <gabor.szaniszlo82@gmail.com>
2019-02-18 22:26:57 +0100

    result controller improved

--- controllers/testResults.js
+++ controllers/testResults.js
@@ -11,78 +11,25 @@ testResults.get('/', (req, res) => {
   });
 });
 
-/*
 // choosenanswers[], userId, resultId, status
-testResults.post('/user/:userId/results/:id/fill', (req, res) => {
-  models.Result.create({userId: req.params.userId, id: req.params.id, status: req.body.status})
+testResults.post('/user/:userId/results/:id/fill', async (req, res) => {
+  let promises = [];
+  models.Result.create({testId: req.params.testId, userId: req.params.userId, status: req.body.status})
     .then(result => {
       req.body.choosenAnswers.forEach(async choosenAnswer => {
-        models.choosenAnswer.create({
-          resultId: result.id,
-          answerId: choosenAnswer.id,
-          points: 0
-        });
-      }),
-    
-    .catch();
-});*/
-
-// create
-testResults.post('/', async (req, res) => {
-  let questionsBlock = req.body.questions;            // a tesztben található összes kérdés
-  let answersBlock = req.body.answers;                // a tesztben a található összes kérdés összes lehetséges válasza sorrendben
-  let choosenAnswersBlock = req.body.choosenAnswers;  // a vizsgázó által kiválasztott válaszok
-
-  let testObject = {
-    userId: req.body.userId,
-    testId: req.body.testId,
-    status: req.body.status,
-    archivedTest: {
-      questions: [],
-      pointsAchieved: 0
-    }
-  };
-
-  for (let i = 0; i < questionsBlock.length; i++) {
-    let questionObject = {
-      number: i,
-      answers: [],
-      choosenAnswer: null,
-      subjectId: questionsBlock[i].subjectId,
-      text: questionsBlock[i].text,
-      value: questionsBlock[i].value,
-      pointsGiven: 0
-    };
-    for (let j = i; j <= i + 3; j++) {
-      questionObject.answers.push(answersBlock[j]);
-    }
-    if (choosenAnswersBlock[i].isCorrect) {
-      questionObject.pointsGiven = questionsBlock[i].value;
-      testObject.archivedTest.pointsAchieved += questionsBlock[i].value;
-    }
-    testObject.archivedTest.questions.push(questionObject);
-    testObject.archivedTest.questions[i].choosenAnswer = choosenAnswersBlock[i];
-  }
-
-  let promises = [];
-  models.Result.create({
-    testObject
-  }).then(async result => {
-    result.questions.map(async question => {
-      promises.push(models.choosenAnswer.create(
-        {
-          resultId: result.id,
-          answerId: question.choosenAnswer.id,
-          points: question.pointsGiven
-        }
-      ));
+        promises.push(
+          models.choosenAnswer.create({
+            resultId: result.id,
+            answerId: choosenAnswer.id,
+            points: choosenAnswer.points
+          }));
+      });
+    })
+    .catch(error => {
+      res.status(500).res.json(error);
     });
-
-    let resp = await Promise.all(promises);
-    res.json({resp});
-  }).catch(error => {
-    res.status(404).json(error);
-  });
+  let resp = await Promise.all(promises);
+  res.json({resp});
 });
 
 module.exports = testResults;
